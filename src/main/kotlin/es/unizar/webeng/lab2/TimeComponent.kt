1cece7dc59750ae90306833e459274dac59399b2
Marcos1236 <marcosgomamartinez@gmail.com>
2025-09-26 09:04:00 +0200

    feature(time): añadir time component, compuesto por un controller, DTO y service

---
+++ src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
@@ -0,0 +1,29 @@
+package es.unizar.webeng.lab2
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@RestController
+class TimeController(private val service: TimeProvider) {
+
+    @GetMapping("/time")
+    fun time(): TimeDTO = service.now().toDTO()
+}
+
+
+data class TimeDTO(val time: LocalDateTime)
+
+fun LocalDateTime.toDTO(): TimeDTO = TimeDTO(time = this)
+
+
+interface TimeProvider {
+    fun now(): LocalDateTime
+}
+
+
+@Service
+class TimeService : TimeProvider {
+    override fun now(): LocalDateTime = LocalDateTime.now()
+}
